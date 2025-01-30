0a687afe5f21757b6d751c271660231f7ff99543
Jaderiel <jabuela22@gmail.com>
2025-01-30 09:46:20 +0800

    added tailwind CLI to the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-// import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>EFS Store</h1>
-    </div>
-  );
+import { Navbar } from './components'
+
+const App = () => {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="font-bold text-orange-500">navbar</div>
+    )
+}
+
+export default Navbar
