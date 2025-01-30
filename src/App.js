b1935e53db0be91af3c4c8af3d5a39cc9e7a7410
Jaderiel <jabuela22@gmail.com>
2025-01-30 11:16:51 +0800

    add gsap animation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import { Navbar } from './components'
-import { Header } from './container'
+import { Header, Popular } from './container'
 
 const App = () => {
     return (
         <div>
             <Navbar />
             <Header />
+            <Popular />
         </div>
     )
 }
--- src/container/header/Header.jsx
+++ src/container/header/Header.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
-import cover from '../../assets/cover.png'
+import React, { useEffect, useRef, useState } from 'react';
+import { gsap } from 'gsap';
+import cover from '../../assets/cover.png';
 
-const header = () => {
+const Header = () => {
+    const imgRef = useRef(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    const handleImageLoad = () => {
+        setIsLoaded(true);
+    };
+
+    useEffect(() => {
+        if (isLoaded) {
+            gsap.fromTo(
+                imgRef.current,
+                { opacity: 0, scale: 1.2 }, // Start slightly zoomed out
+                { opacity: 1, scale: 1, duration: 2, ease: 'power2.out' } // Fade in and zoom in
+            );
+        }
+    }, [isLoaded]);
+
     return (
         <div className="relative">
-            <img 
-                src={cover} 
-                alt="Header Cover" 
-                className="w-full h-screen object-cover" 
+            <img
+                ref={imgRef}
+                src={cover}
+                alt="Header Cover"
+                className="w-full h-screen object-cover"
+                onLoad={handleImageLoad}
             />
         </div>
-    )
-}
+    );
+};
 
-export default header
+export default Header;
