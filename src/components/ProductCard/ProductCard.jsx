948cd3a5314f32249200919b9cfed5cb6e494a9c
Jaderiel <jabuela22@gmail.com>
2025-02-06 12:11:34 +0800

    add cursor pointer on divs

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -5,12 +5,12 @@ const ProductCard = () => {
         <div>
             <div className='bg-blue-300 w-full h-120'>1</div>
             <div className='flex justify-between items-center'>
-                <div>
+                <div className='cursor-pointer'>
                     <div>Product Name</div>
                     <div>Price</div>
                 </div>
                 <div className='mr-2'>
-                    <i className='pi pi-heart'></i>
+                    <i className='pi pi-heart cursor-pointer'></i>
                 </div>
             </div>
         </div>
